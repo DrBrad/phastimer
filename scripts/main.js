ec2522370e55c7fba56b7a2300d90fef5a9a8208
DrBrad <brad@bradeagle.com>
2026-01-27 19:48:34 -0700

    Working settings

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,16 @@ const ghostSpeed = document.getElementById('ghost-speed');
 const bloodMoon = document.getElementById('blood-moon');
 const state = new TapState();
 
-var smudge_timer_running = false, obombo_state = false, new_state = false, obombo_timer_running = false;
-var smudge_now = 0, obombo_now = 0;
+const SPEEDS = [
+    0.5,
+    0.75,
+    1.0,
+    1.25,
+    1.50
+];
+
+var smudge_timer_running = false, obombo_state = false, new_state = false, obombo_timer_running = false, ghost_speed_toggle;
+var smudge_now = 0, obombo_now = 0, ghost_speed = 2;
 
 smudge.onclick = function(e){
     smudge_now = new Date().getTime();
@@ -33,7 +41,7 @@ obombo.onclick = function(e){
 ms.onclick = function(e){
     const out = state.tapAndCompute();
     if(out){
-        let ms = out.ms;
+        let ms = out.ms/SPEEDS[ghost_speed];
 
         if(bloodMoon.checked){
             ms = ms*0.85;
@@ -70,6 +78,66 @@ setInterval(function(){
         }
     }
 }, 16);
+
+ghostSpeed.onmousedown = function(event){
+    ghost_speed_toggle = true;
+};
+
+window.onmousemove = function(event){
+    if(ghost_speed_toggle){
+        let percent = (((event.clientX-ghostSpeed.getBoundingClientRect().left)/ghostSpeed.clientWidth)*100);
+        ghost_speed = getGhostSpeed(percent);
+        ghostSpeed.style.setProperty('--seek', getRelativeSeek(percent)+'%');
+    }
+};
+
+window.onmouseup = function(event){
+    ghost_speed_toggle = false;
+};
+
+ghostSpeed.onclick = function(event){
+    let percent = (((event.clientX-this.getBoundingClientRect().left)/this.clientWidth)*100);
+    ghost_speed = getGhostSpeed(percent);
+    this.style.setProperty('--seek', getRelativeSeek(percent)+'%');
+};
+
+function getRelativeSeek(num){
+    switch(true){
+        case (num >= 0 && num < 12.5):
+            return 0;
+
+        case (num >= 12.5 && num < 37.5):
+            return 25;
+
+        case (num >= 37.5 && num < 62.5):
+            return 50;
+
+        case (num >= 62.5 && num < 87.5):
+            return 75;
+
+        default:
+            return 100;
+    }
+}
+
+function getGhostSpeed(num){
+    switch(true){
+        case (num >= 0 && num < 12.5):
+            return 0;
+
+        case (num >= 12.5 && num < 37.5):
+            return 1;
+
+        case (num >= 37.5 && num < 62.5):
+            return 2;
+
+        case (num >= 62.5 && num < 87.5):
+            return 3;
+
+        default:
+            return 4;
+    }
+}
 
 function msToMsm(ms){
     let totalSeconds = Math.floor(ms / 1000);
